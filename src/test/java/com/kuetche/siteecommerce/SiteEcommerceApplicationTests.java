5d70e5fa420071fdcb6a96939bda1fcdf8ba86eb
kuetcheal <akuetche55@gmail.com>
2026-05-26 10:34:50 +0200

    initialize projet site e-commerce

---
+++ src/test/java/com/kuetche/siteecommerce/SiteEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuetche.siteecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
